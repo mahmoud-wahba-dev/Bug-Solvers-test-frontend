026f5caba34c207f0ae2dd17f0d5d1d608e3face
Mahmoud Wahba <mahmoud.wahba.dev@gmail.com>
2024-07-09 16:55:31 +0300

    update website content and fix issues

--- content/js/owl-init.js
+++ content/js/owl-init.js
@@ -1,57 +1,22 @@
 $(document).ready(function () {
-  function initializeOwlCarousel() {
-    var autoplayValue = $(window).width() >= 1000 ? false : true;
-    var owl = $(".owl-carousel").owlCarousel({
-      // loop: true,
-      margin: 20,
-      nav: false,
-      items: 1,
-      autoplay: true,
-      autoplayTimeout: 2000,
-      autoplayHoverPause: true,
-      rtl: false, // Enable RTL mode
-      dots: true,
-      responsive: {
-        0: {
-          items: 1,
-        },
-        600: {
-          items: 2,
-        },
-        1000: {
-          items: 3,
-        },
-      },
-    });
-
-    // Update autoplay on window resize
-    $(window).on("resize", function () {
-      var newAutoplayValue = $(window).width() >= 1000 ? false : true;
-      owl.trigger("destroy.owl.carousel"); // Destroy the carousel
-      $(".owl-carousel").owlCarousel({
-        loop: true,
-        margin: 20,
-        nav: false,
+  $(".owl-carousel").owlCarousel({
+    loop: true, // Enable loop mode
+    margin: 20,
+    nav: false, // Enable navigation buttons
+    items: 1, // Number of items per slide
+    autoplay: true, // Autoplay enabled
+    autoplayTimeout: 2000, // Autoplay interval in milliseconds (3 seconds)
+    autoplayHoverPause: false, // Autoplay continues even when hovering over carousel
+    responsive: {
+      0: {
         items: 1,
-        autoplay: newAutoplayValue,
-        autoplayTimeout: 2000,
-        autoplayHoverPause: true,
-        rtl: false, // Enable RTL mode
-        dots: true,
-        responsive: {
-          0: {
-            items: 1,
-          },
-          600: {
-            items: 2.1,
-          },
-          1000: {
-            items: 3.4,
-          },
-        },
-      });
-    });
-  }
-
-  initializeOwlCarousel();
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
 });
